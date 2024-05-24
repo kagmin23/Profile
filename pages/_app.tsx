24f6666f611eff9f0cf64ce44d7d72bfbe78fa44
KagMin <phankangmin@gmail.com>
2024-05-24 10:56:14 +0700

    mmm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
+import { AppProps } from 'next/app';
 import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <div className="grid grid-cols-12 gap-6 px-5 px-48 lg:px-48 my-14">
-          <div className="col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2x1">
-            <Sidebar/>
-            </div>
-          <div className="col-span-12 bg-white lg:col-span-3 rounded-2x1">
-              <Component {...pageProps} />
-          </div>
+    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14">
+      <div className="col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-xl">
+        <Sidebar />
       </div>
-  )
+      <div className="col-span-12 bg-white lg:col-span-9 rounded-xl">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
